092c1dfdc8d548225eb961ba7bfa7f706d7654ae
Richardgvaf <noticiasnuevayo@gmail.com>
2021-01-10 10:40:25 -0600

    added  angular client

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8081;
 
 app.get('/', (req, res) => res.send('Hello Isennya!'));
-
+app.get('/users',(req,res) => res.send('hello User'))
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
